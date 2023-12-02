c1a6335df33a98273acbd741a7a6ecb77f0ec962
1p22geo <1p22geodecki@gmail.com>
2023-12-02 13:13:32 +0100

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bartoszg/pehap',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e1b9502d4313d93356e58f5b2e7afd73f950ff3e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bartoszg/pehap' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e1b9502d4313d93356e58f5b2e7afd73f950ff3e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
