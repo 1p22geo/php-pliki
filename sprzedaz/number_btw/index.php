cb928d11e383abc4cbabf00dd50b86613e7d8c8d
1p22geo <1p22geodecki@gmail.com>
2023-12-02 14:42:02 +0100

    sprzedaż jest gotowa.

---
+++ sprzedaz/number_btw/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Wypisz transakcje</title>
+</head>
+<body class="bg-slate-400">
+    <a href="/sprzedaz/">BACK</a>
+    <?php 
+if(!isset($_GET["start"]) || !isset($_GET["stop"])){
+  echo "<h1>Proszę wybrać zakres czasu</h1>
+    <form action='/sprzedaz/number_btw/' method='GET'>
+
+<label for=start>
+            start:
+            <input id=start name=start type=date><br>
+        </label> 
+ <label for=stop>
+            stop:
+            <input id=stop name=stop type=date><br>
+        </label> 
+       
+        <input type=submit value=submit>
+
+</form>    
+    ";
+    }
+else{
+
+  function ft(Transaction $tr){
+    if($tr->date->format("Y-m-d")>$_GET["start"] && $tr->date->format("Y-m-d")<$_GET['stop']){
+      return true;
+    }
+  }
+
+    require("../model/transaction.php");
+
+    $transactions = Transaction::load();
+    $transactions = (array_filter($transactions, "ft"));
+    $ct = count($transactions);
+    $st = $_GET["start"];
+    $tp = $_GET["stop"];
+    echo "<h1 class='text-xl font-bold'>Było $ct transakcji między $st a $tp</h1>";
+
+
+    echo "<table class='w-1/3 bg-slate-600 shadow-md rounded px-8 pt-6 pb-8 mb-4'><tr class='p-2'><th class='p-2'>Produkt</th> <th class='p-2'>Cena</th> <th class='p-2'>Data</th> <th class='p-2'>ID</th></tr>";
+
+    foreach ($transactions as $tr) {
+        $date = $tr->date->format("Y-m-d");
+        $price = strval($tr->price);
+    echo "<tr>
+    <td class='p-2'>$tr->product</td>
+    <td class='p-2'>$price</td>
+    <td class='p-2'>$date</td>
+    <td class='p-2'>$tr->id</td>
+    </tr>";
+    }
+
+    echo "</table>";
+}
+    ?>
+
+</body>
+</html>
